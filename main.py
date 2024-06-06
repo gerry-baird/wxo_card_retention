a8705a4d8dd5bbff2b97eabc1e4437eba6720e78
Gerry Baird <gerry.baird@uk.ibm.com>
2024-06-06 12:33:52 +0100

    Many and random

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import random
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.routing import APIRoute
@@ -5,6 +7,8 @@ from fastapi.security import HTTPBasic
 
 app = FastAPI()
 security = HTTPBasic()
+
+
 class AccountSummary(BaseModel):
     id: str
     fee: float
@@ -13,16 +17,50 @@ class AccountSummary(BaseModel):
     transactions: float
 
 
+class AccountSummaryList(BaseModel):
+    summaries: list[AccountSummary]
+
+
+def create_account_summaries():
+    return [
+        AccountSummary(id="A1234", fee=200, interest=350, points=250, transactions=5000),
+        AccountSummary(id="L1234", fee=100, interest=50, points=500, transactions=1000),
+        AccountSummary(id="V1234", fee=350, interest=4500, points=250, transactions=2500000),
+        AccountSummary(id="H1234", fee=200, interest=500, points=2000, transactions=25000),
+        AccountSummary(id="N1234", fee=200, interest=500, points=200, transactions=10000),
+        AccountSummary(id="G1234", fee=100, interest=100, points=0, transactions=30000)
+    ]
+
+
+# this is our datastore
+account_summaries = create_account_summaries()
+
+@app.get("/account_summaries",
+         summary='View all account summaries',
+         description='View all account summaries',
+         response_description="Account summaries",
+         tags=["Summaries"]
+         )
+def get_summaries() -> AccountSummaryList:
+    all_summaries = AccountSummaryList(summaries=account_summaries)
+    return all_summaries
 
 @app.get("/account_summary/{id}",
          summary='Account Summary',
          description='Get a summary of account activity',
          )
 async def get_account_summary() -> AccountSummary:
+    for acc in account_summaries:
+        if acc.id == id:
+            return acc
 
-    summary = AccountSummary(id="A1234", fee=200, interest=350, points=250, transactions=5000)
+    # Create a random account
+    rnd_fee = random(50,300)
+    rnd_interest = random(50, 3000)
+    rnd_points = random(500, 5000)
+    rnd_tx = random(1500, 50000)
 
-    return summary
+    summary = AccountSummary(id=id, fee=rnd_fee, interest=rnd_interest, points=rnd_points, transactions= rnd_tx)
 
 
 def use_route_names_as_operation_ids(app: FastAPI) -> None:
